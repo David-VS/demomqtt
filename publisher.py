60f49e9c4ab8ef787b0be958c4d1d53324091b5a
David-VS <63110701+David-VS@users.noreply.github.com>
2024-11-06 16:11:05 +0100

    added example to send json data

--- publisher.py
+++ publisher.py
@@ -1,5 +1,9 @@
 from paho.mqtt import client
 import time
+import json
+import dataclasses
+import sensors as s
+import random
 
 
 broker = 'd026818d.ala.eu-central-1.emqxsl.com'
@@ -16,7 +20,7 @@ def connect():
         else:
             print("connection failed for reasons")
 
-    my_client = client.Client(client.CallbackAPIVersion.VERSION2)
+    my_client = client.Client(client_id="joske", callback_api_version=client.CallbackAPIVersion.VERSION2)
     my_client.tls_set(ca_certs='./certificates/emqxsl-ca.crt')
     my_client.username_pw_set(username, password)
     my_client.connect(broker, port)
@@ -28,8 +32,14 @@ def publish(my_client):
 
     while True:
         time.sleep(2)
-        msg = input("message = ")
+        sensor_name = input("Name of the sensor= ")
+        sensor_type = input("Type of the sensor= ")
+        sensor_value = random.randint(-10,30)
+
+        sensor = s.Sensor(sensor_name, sensor_type, sensor_value)
+
         topic = "prog3/mqtt"
+        msg = json.dumps(dataclasses.asdict(sensor))
 
         result = my_client.publish(topic, msg)
         if result[0] == 0:
@@ -40,7 +50,9 @@ def publish(my_client):
 
 def run():
     my_client = connect()
+    my_client.loop_start()
     publish(my_client)
+    my_client.loop_stop()
 
 
 if __name__ == '__main__':
